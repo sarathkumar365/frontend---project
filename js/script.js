7254ebb38a26a89cac070fe895f61ba1f091ef62
sarathkumarks365 <sarathkumark.s365@gmail.com>
2023-12-12 21:36:51 -0500

    admin login via signup implemented

--- js/script.js
+++ js/script.js
@@ -34,7 +34,43 @@ const currentUser = {
 
 console.log(currentUser);
 
-// sign logic
+// signin logic
+
+// function checkCredentials(event) {
+//     console.log('signin');
+//     event.preventDefault();
+
+//     // Get entered credentials
+//     const enteredEmail = document.getElementById('loginEmail').value;
+//     const enteredPassword = document.getElementById('loginPassword').value;
+
+//     // Get stored data from cookies
+//     const storedData = getCookie('formData');
+
+//     if (storedData) {
+//         // Parse stored data
+//         const storedFormData = JSON.parse(storedData);
+
+//         // Check if entered credentials match stored data
+//         if (enteredEmail === storedFormData.email && enteredPassword === storedFormData.password) {
+
+//             currentUser.name = storedFormData.name;
+//             currentUser.isLoggedIn = true;
+//             // Store form data in browser cookies as JSON
+//             setCookie('currentUser', JSON.stringify(currentUser), 365);
+           
+//             alert('Login successful! Redirecting to the dashboard.');
+
+
+//             // Redirect to the dashboard or another page after successful login
+//             window.location.href = '../html/allvehicles.html';
+//         } else {
+//             alert('Invalid credentials. Please try again.');
+//         }
+//     } else {
+//         alert('No user data found. Please sign up.');
+//     }
+// }
 
 function checkCredentials(event) {
     console.log('signin');
@@ -43,6 +79,14 @@ function checkCredentials(event) {
     // Get entered credentials
     const enteredEmail = document.getElementById('loginEmail').value;
     const enteredPassword = document.getElementById('loginPassword').value;
+
+    // Check for admin credentials
+    if (enteredEmail === 'admin@gmail.com' && enteredPassword === '123') {
+        // Admin login successful
+        alert('Admin login successful! Redirecting to the admin page.');
+        window.location.href = '../html/admin.html';
+        return; // Exit the function to avoid executing the user login code
+    }
 
     // Get stored data from cookies
     const storedData = getCookie('formData');
@@ -53,24 +97,27 @@ function checkCredentials(event) {
 
         // Check if entered credentials match stored data
         if (enteredEmail === storedFormData.email && enteredPassword === storedFormData.password) {
-
             currentUser.name = storedFormData.name;
             currentUser.isLoggedIn = true;
-            // Store form data in browser cookies as JSON
-            setCookie('currentUser', JSON.stringify(currentUser), 365);
-           
-            alert('Login successful! Redirecting to the dashboard.');
 
+            // Store form data in browser cookies as JSON (consider more secure methods)
+            setCookie('currentUser', JSON.stringify(currentUser), 365);
+
+            // Consider using a modal or other UI elements for success messages
+            alert('User login successful! Redirecting to the dashboard.');
 
             // Redirect to the dashboard or another page after successful login
             window.location.href = '../html/allvehicles.html';
         } else {
+            // Consider using a modal or other UI elements for error messages
             alert('Invalid credentials. Please try again.');
         }
     } else {
+        // Consider using a modal or other UI elements for informative messages
         alert('No user data found. Please sign up.');
     }
 }
+
 
 // logout
 
@@ -112,54 +159,6 @@ deleteCookie('userToken');
 
 
 // checkout functionality
-
-// new save checkout data functionality
-
-// function submitForm() {
-//     try {
-//         // Get the values from the form
-//         const selectedDay = document.getElementById('day').value;
-//         const duration = document.getElementById('duration').value;
-
-//         // Create an object to store new form data
-//         const newFormData = {
-//             selectedDay: selectedDay,
-//             duration: duration
-//         };
-
-//         // Retrieve existing data from the cookie
-//         let existingData = getExistingFormData();
-
-//         // Ensure existingData is an array
-//         if (!Array.isArray(existingData)) {
-//             existingData = [];
-//         }
-
-//         // Append the new data to the existing array
-//         existingData.push(newFormData);
-
-//         // Convert the updated array to a JSON string
-//         const updatedFormDataJSON = JSON.stringify(existingData);
-
-//         // Store the updated JSON string in a cookie
-//         document.cookie = `reservationData=${updatedFormDataJSON}; expires=${new Date(Date.now() + 86400000).toUTCString()}; path=/`;
-
-//         // Log to console (you can remove this in a real application)
-//         console.log(`Form data stored in cookie: ${updatedFormDataJSON}`);
-
-//         // Display success message
-//         alert('Form submitted successfully!');
-
-//         // Navigate to index.html after a delay
-//         setTimeout(function () {
-//             window.location.href = '../index.html';
-//         }, 1000);
-//     } catch (error) {
-//         console.error('Error submitting form:', error);
-//         // Handle the error, show an error message, etc.
-//     }
-// }
-
 
 
 // // Helper function to retrieve existing form data from the cookie
